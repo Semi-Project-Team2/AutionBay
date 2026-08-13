62dad110daf7a9928d47f36bab225ed4cde5fd7f
sjlimiei26 <sjlim.iei.26@gmail.com>
2026-08-13 10:55:20 +0900

    260813 아이디 중복확인 이벤트 처리

--- 03_community/src/main/resources/static/js/member.js
+++ 03_community/src/main/resources/static/js/member.js
@@ -30,3 +30,85 @@ profileImage.addEventListener('change', function(e) {
 
 
 
+// 비밀번호 일치 여부 확인
+const memberPwd = document.querySelector("#member-pwd");				// 비밀번호 입력창
+const memberPwdConfirm = document.querySelector("#member-pwd-confirm");	// 비밀번호 확인 입력창
+
+let checkPwd = false;	// 비밀번호 일치 여부
+
+function validatePwdConfirm() {
+	const confirmResult = document.querySelector("#check-pwd-result");
+	
+	// 비밀번호 확인 입력창이 비어있을 경우 검사 x
+	if (!memberPwdConfirm.value.trim()) {
+		confirmResult.textContent = "";
+		checkPwd = false;
+		return;
+	}
+	
+	checkPwd = memberPwd.value === memberPwdConfirm.value;
+	
+	confirmResult.textContent = checkPwd ? "비밀번호가 일치합니다." : "비밀번호가 일치하지 않습니다.";
+	confirmResult.className = checkPwd ? "form-tip form-tip-ok" : "form-tip form-tip-error";
+}
+
+memberPwd.addEventListener('input', validatePwdConfirm);
+memberPwdConfirm.addEventListener('input', validatePwdConfirm);
+
+const checkIdResult = document.querySelector("#check-id-result");
+const memberIdInput = document.querySelector("#member-id");
+memberIdInput.addEventListener("input", function() {
+	checkIdResult.textContent = "";
+});
+
+// 아이디 [중복확인] 버튼의 클릭 이벤트 리스너 추가 (TODO: alert)
+const checkIdBtn = document.querySelector("#check-id-btn");
+checkIdBtn.addEventListener("click", function() {
+	const memberId = memberIdInput.value.trim();
+	// 아이디 값이 입력되지 않았을 경우, 요청 x
+	if (memberId.length === 0) {
+		checkIdResult.textContent = "아이디를 입력해주세요.";
+		checkIdResult.className = "form-tip form-tip-error";
+		return;
+	}
+	
+	// 입력된 아이디값이 중복되는 지 서버로 요청!
+	/*
+		* fetch API
+		  : 브라우저에서 서버로 요청을 보내고 응답을 받을 수 있게 해주는 자바스크립트 내장 함수
+		    form 태그의 submit과 달리 "화면 새로고침 없이(비동기적으로)", 
+			백엔드 서버와 데이터를 주고 받을 수 있음. 이러한 통신 방식을 AJAX라고 함.
+			
+		fetch(URL, settings)
+		- URL : 요청을 보낼 주소
+		- settings : 설정 객체 (요청 방식, 헤더, 데이터 등)
+		  - method: 요청 방식
+		  - headers: 헤더 설정 
+	*/
+	
+});
+
+
+// 회원가입 폼 제출 => 비밀번호가 일치했을 때 제출하도록 처리
+const joinForm = document.querySelector("#join-form");
+joinForm.addEventListener("submit", function(e) {
+	
+	if (!checkPwd) {
+		e.preventDefault();		// 기존 폼 제출 동작을 막기!
+		alert("비밀번호가 일치하지 않습니다.");
+		return;
+	}
+	
+});
+
+
+
+
+
+
+
+
+
+
+
+
