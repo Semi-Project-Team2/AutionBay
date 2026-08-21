66b866f8cde77c0f6dd02b34e9665fef3c3adf11
Monica6788 <kekelee0070@gmail.com>
2026-08-21 15:12:30 +0900

    어플리케이션 프로퍼티 깃이그노어 수정

--- auctionBay/src/main/java/com/kh/auctionBay/user/controller/MyPageController.java
+++ auctionBay/src/main/java/com/kh/auctionBay/user/controller/MyPageController.java
@@ -1,5 +1,13 @@
 package com.kh.auctionBay.user.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypage")
 public class MyPageController {
+	
+	/* 화면 이동 요청 */
+	
 
 }
