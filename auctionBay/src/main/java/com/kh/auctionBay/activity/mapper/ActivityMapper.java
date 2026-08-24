149f72fd0503492aaacf4ab91cae6c691451deba
ongshu8027-svg <ongshu8027@gmail.com>
2026-08-24 11:49:23 +0900

    작업 중인 내 코드 중간 저장

--- auctionBay/src/main/java/com/kh/auctionBay/activity/mapper/ActivityMapper.java
+++ auctionBay/src/main/java/com/kh/auctionBay/activity/mapper/ActivityMapper.java
@@ -2,13 +2,14 @@ package com.kh.auctionBay.activity.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.kh.auctionBay.activity.dto.MyBoardDTO;
 import com.kh.auctionBay.activity.dto.MyCommentDTO;
 import com.kh.auctionBay.activity.dto.RecentViewDTO;
 import com.kh.auctionBay.activity.dto.WishlistDTO;
-
+@Mapper
 public interface ActivityMapper {
 	List<MyBoardDTO> selectMyBoardList(Long userNo);
 	//2. 회원 번호(userNo)를 받아 해당 회원이 작성한 댓글 목록을 조회하는 메서드
